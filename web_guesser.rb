9d97427f9aad1ffafa1f048454f2f994b95d8cba
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-09-26 20:00:09 +0200

    web guesser

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,9 +1,54 @@
-require 'sinatra'
-require 'sinatra/reloader'
+require "sinatra"
+require "sinatra/reloader"
 
 
-secret_num = rand(100)
+class WebGuesser
+  attr_reader :secret_number
+  attr_accessor :color, :guesses
+
+  def initialize
+    @secret_number = rand(99) + 1
+    @color = "Cornflower Blue"
+    @@guesses = 5
+  end
+
+  def restart
+    @secret_number = rand(99) + 1
+    @@guesses = 5
+  end
+
+  def set_color(color)
+    @color = color
+  end
+
+  def check_guess(guess, secret_number)
+    @@guesses -= 1
+    if @@guesses == 0
+      restart
+      return "Sorry! Out of guesses! Try again."
+    end
+    if guess == secret_number
+      set_color("Green")
+      "Correct! \nThe SECRET NUMBER is #{secret_number}."
+    elsif guess > (secret_number + 5)
+      set_color("Red")
+      "Way too high!"
+    elsif guess < (secret_number - 5)
+      set_color("Red")
+      "Way too low!"
+    elsif guess > secret_number
+      set_color("#ffb3b3")
+      "Too high!"
+    elsif guess < secret_number
+      set_color("#ffb3b3")
+      "Too low!"
+    end
+  end
+end
+
+guesser = WebGuesser.new
 get '/' do
-  erb :index, :locals => {:number => secret_num}
-  throw params.inspect
+  guess = params["guess"].to_i
+  message = guesser.check_guess(guess, guesser.secret_number)
+  erb :index, :locals => {:secret_number => @secret_number, :message => message, :color => guesser.color}
 end
